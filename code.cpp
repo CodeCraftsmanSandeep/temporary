e1de690c09719e062beb89b1ffb655138825136e
sandeep <161121811+CodeCraftsmanSandeep@users.noreply.github.com>
2025-07-04 11:01:45 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,278 +1,263 @@
 #pragma once
 
-#include "vrp.h"
+/*
+DEPOT is 0
+Customers: 1 to size-1 inclusive
+*/
 
-void tsp_approx(const CVRP &vrp, std::vector<node_t> &cities, std::vector<node_t> &tour, node_t ncities) {
-  node_t i, j;
-  node_t ClosePt = 0;
-  weight_t CloseDist;
-  //~ node_t endtour=0;
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+#include <sstream>  //stringstream
+#include <random>
+#include <algorithm>
+#include <stack>
+#include <cfloat>
+#include <queue>
+#include <chrono>  //timing CPU
 
-  for (i = 1; i < ncities; i++)
-    tour[i] = cities[i - 1];
 
-  tour[0] = cities[ncities - 1];
+// Error handling
+#define ERROR_FILE std::cerr
+#define HANDLE_ERROR(message) \
+    do { \
+        ERROR_FILE << "❌ Error: " << (message) << "\n" \
+                   << "   In file: " << __FILE__ << "\n" \
+                   << "   At line: " << __LINE__ << std::endl; \
+        std::exit(EXIT_FAILURE); \
+    } while (0)
 
-  for (i = 1; i < ncities; i++) {
-    //~ double ThisX = points.x_coords[tour[i-1]];
-    //~ double ThisY = points.y_coords[tour[i-1]];
-    weight_t ThisX = vrp.node[tour[i - 1]].x;
-    weight_t ThisY = vrp.node[tour[i - 1]].y;
-    CloseDist = DBL_MAX;
-    for (j = ncities - 1;; j--) {
-      weight_t ThisDist = (vrp.node[tour[j]].x - ThisX) * (vrp.node[tour[j]].x - ThisX);
-      if (ThisDist <= CloseDist) {
-        ThisDist += (vrp.node[tour[j]].y - ThisY) * (vrp.node[tour[j]].y - ThisY);
-        if (ThisDist <= CloseDist) {
-          if (j < i)
-            break;
-          CloseDist = ThisDist;
-          ClosePt = j;
-        }
-      }
-    }
-    /*swapping tour[i] and tour[ClosePt]*/
-    unsigned temp = tour[i];
-    tour[i] = tour[ClosePt];
-    tour[ClosePt] = temp;
+// Output file
+#define OUTPUT_FILE std::cout
+
+// Constants
+constexpr double PI = 3.14159265358979323846;
+
+// Types
+using cord_t = double;       // Type for x and y co-ordinates
+using weight_t = double;     // Type of weight between two 
+using demand_t = double;
+using capacity_t = double;
+using node_t = int32_t;
+
+// Point in the 2-D plane: (customer (or) depot)
+class Point
+{
+public:
+    cord_t x;
+    cord_t y;
+    demand_t demand;
+
+    Point() {}
+    Point(cord_t _x, cord_t _y, demand_t _demand): x(_x), y(_y), demand(_demand) {}
+    ~Point() {}
+};
+
+class CVRP
+{
+    void read (const std::string filename);
+public:
+    capacity_t capacity;
+    size_t size;
+    std::vector <Point> node;
+    std::string type;
+    const node_t depot = 0; // this is must
+    std::vector<weight_t> dist; // distance matrix
+
+    CVRP (const std::string filename);
+    void print ();
+    double get_distance_on_the_fly(node_t, node_t) const;
+    ~CVRP () {}
+};
+
+weight_t CVRP::get_distance_on_the_fly(node_t u, node_t v) const
+{
+  if (u < 0 || u >= size || v < 0 || v >= size) {
+    HANDLE_ERROR("Node index out of bounds");
   }
+  if (u == v) {
+    return 0.0; // Distance to itself is zero
+  }
+
+  // Euclidian distance calculating on the fly
+  return sqrt((node[u].x - node[v].x) * (node[u].x - node[v].x) + (node[u].y - node[v].y) * (node[u].y - node[v].y));
+}
+
+CVRP::CVRP(const std::string filename)
+{
+    read(filename);
+}
+
+void CVRP::read(const std::string filename) 
+{
+  std::ifstream in(filename);
+  if (!in.is_open()) {
+    std::cerr << "Could not open the file \"" << filename << "\"" << std::endl;
+    exit(1);
+  }
+  std::string line;
+  for (int i = 0; i < 3; ++i)
+    getline(in, line);
+
+  // DIMENSION
+  getline(in, line);
+  size = stof(line.substr(line.find(":") + 2));
+
+
+  // DISTANCE TYPE
+  getline(in, line);
+  type = line.find(":");
+
+  // CAPACITY
+  getline(in, line);
+  capacity = stof(line.substr(line.find(":") + 2));
+
+  //skip NODE_COORD_SECTION
+  getline(in, line);
+
+  // Allocate
+  node.resize(size);
+
+  //~ 1  x1  y1
+  //~ 2  x2  y2
+  //~ 3  x3  y3
+  //~ ...
+  //~ n  xn  yn
+
+  for (size_t i = 0; i < size; ++i) {
+    getline(in, line);
+
+    std::stringstream iss(line);
+    size_t id;
+    std::string xStr, yStr;
+
+    iss >> id >> xStr >> yStr;
+    node[i].x = stof(xStr);
+    node[i].y = stof(yStr);
+  }
+
+  // skip DEMAND_SECTION
+  getline(in, line);
+
+  for (size_t i = 0; i < size; ++i) {
+    getline(in, line);
+    std::stringstream iss(line);
+
+    size_t id;
+    std::string dStr;
+    iss >> id >> dStr;
+
+    node[i].demand = stof(dStr);
+  }
+  in.close();
+
+  return;
+}
+
+void CVRP::print()
+{
+    OUTPUT_FILE << "SIZE: " << size << "\n";
+    OUTPUT_FILE << "Capacity: " << capacity << "\n";
+    OUTPUT_FILE << std::setw(6) << "NODE" 
+                << std::setw(10) << "X"  
+                << std::setw(10) << "Y" 
+                << std::setw(10) << "DEMAND" << "\n";
+
+    for (size_t i = 0; i < size; ++i) {
+        OUTPUT_FILE << std::setw(6) << i
+                    << std::setw(10) << node[i].x 
+                    << std::setw(10) << node[i].y 
+                    << std::setw(10) << node[i].demand 
+                    << "\n";
+    }
+    return;
 }
 
 
-std::vector<std::vector<node_t>>
-postprocess_tsp_approx(const CVRP &vrp, const std::vector<std::vector<node_t>> &solRoutes) {
-  std::vector<std::vector<node_t>> modifiedRoutes;
+// Print in DIMACS output format http://dimacs.rutgers.edu/programs/challenge/vrp/cvrp/
+// Depot is 0
+// Route #1: 1 2 3
+// Route #2: 4 5
+// ...
+// Route #k: n-1 n
+//
+void print_routes(const std::vector<std::vector<node_t>> &final_routes, const weight_t final_cost) 
+{
 
-  unsigned nroutes = solRoutes.size();
-  for (unsigned i = 0; i < nroutes; ++i) {
-    // postprocessing solRoutes[i]
-    unsigned sz = solRoutes[i].size();
-
-    std::vector<node_t> cities(sz + 1);
-    std::vector<node_t> tour(sz + 1);
-
-    for (unsigned j = 0; j < sz; ++j)
-      cities[j] = solRoutes[i][j];
-
-    cities[sz] = 0;  // the last node is the depot.
-
-    tsp_approx(vrp, cities, tour, sz + 1);
-
-    // the first element of the tour is now the depot. So, ignore tour[0] and insert the rest into the vector.
-
-    std::vector<node_t> curr_route;
-    for (unsigned kk = 1; kk < sz + 1; ++kk) {
-      curr_route.push_back(tour[kk]);
-    }
-
-    modifiedRoutes.push_back(curr_route);
-  }
-  return modifiedRoutes;
-}
-
-void tsp_2opt(const CVRP &vrp, std::vector<node_t> &cities, std::vector<node_t> &tour, unsigned ncities) {
-  // 'cities' contains the original solution. It is updated during the course of the 2opt-scheme to contain the 2opt soln.
-  // 'tour' is an auxillary array.
-
-  // repeat until no improvement is made
-  unsigned improve = 0;
-
-  while (improve < 2) {
-    double best_distance = 0.0;
-
-    best_distance += vrp.get_distance_on_the_fly(vrp.depot, cities[0]);  // computing distance of the first point in the route with the depot.
-
-    for (unsigned jj = 1; jj < ncities; ++jj) {
-      best_distance += vrp.get_distance_on_the_fly(cities[jj - 1], cities[jj]);
-    }
-
-    best_distance += vrp.get_distance_on_the_fly(vrp.depot, cities[ncities - 1]);
-    // 1x 2x 3x 4 5
-    //  1 2  3  4 5
-    for (unsigned i = 0; i < ncities - 1; i++) {
-      for (unsigned k = i + 1; k < ncities; k++) {
-        for (unsigned c = 0; c < i; ++c) {
-          tour[c] = cities[c];
-        }
-
-        unsigned dec = 0;
-        for (unsigned c = i; c < k + 1; ++c) {
-          tour[c] = cities[k - dec];
-          dec++;
-        }
-
-        for (unsigned c = k + 1; c < ncities; ++c) {
-          tour[c] = cities[c];
-        }
-        double new_distance = 0.0;
-
-        new_distance += vrp.get_distance_on_the_fly(vrp.depot, tour[0]);
-        for (unsigned jj = 1; jj < ncities; ++jj) {
-          new_distance += vrp.get_distance_on_the_fly(tour[jj - 1], tour[jj]);
-        }
-
-        new_distance += vrp.get_distance_on_the_fly(vrp.depot, tour[ncities - 1]);
-
-        if (new_distance < best_distance) {
-          // Improvement found so reset
-          improve = 0;
-          for (unsigned jj = 0; jj < ncities; jj++)
-            cities[jj] = tour[jj];
-          best_distance = new_distance;
-        }
-      }
-    }
-    improve++;
-  }
-}
-
-std::vector<std::vector<node_t>>
-postprocess_2OPT(const CVRP &vrp, const std::vector<std::vector<node_t>> &final_routes) {
-  std::vector<std::vector<node_t>> postprocessed_final_routes;
-
-  unsigned nroutes = final_routes.size();
-  for (unsigned i = 0; i < nroutes; ++i) {
-    // postprocessing final_routes[i]
-    unsigned sz = final_routes[i].size();
-
-    std::vector<node_t> cities(sz);
-    std::vector<node_t> tour(sz);
-
-    for (unsigned j = 0; j < sz; ++j)
-      cities[j] = final_routes[i][j];
-
-    std::vector<node_t> curr_route;
-
-    if (sz > 2)                         // for sz <= 1, the cost of the path cannot change. So no point running this.
-      tsp_2opt(vrp, cities, tour, sz);  //MAIN
-
-    for (unsigned kk = 0; kk < sz; ++kk) {
-      curr_route.push_back(cities[kk]);
-    }
-
-    postprocessed_final_routes.push_back(curr_route);
-  }
-  return postprocessed_final_routes;
-}
-
-weight_t get_total_cost_of_routes(const CVRP &vrp, std::vector<std::vector<node_t>> &final_routes) {
-  weight_t total_cost = 0.0;
   for (unsigned ii = 0; ii < final_routes.size(); ++ii) {
-    weight_t curr_route_cost = 0;
-
-    curr_route_cost += vrp.get_distance_on_the_fly(vrp.depot, final_routes[ii][0]);
-    for (unsigned jj = 1; jj < final_routes[ii].size(); ++jj) {
-      curr_route_cost += vrp.get_distance_on_the_fly(final_routes[ii][jj - 1], final_routes[ii][jj]);
+    OUTPUT_FILE << "Route #" << ii + 1 << ":";
+    for (unsigned jj = 0; jj < final_routes[ii].size(); ++jj) {
+      OUTPUT_FILE << " " << final_routes[ii][jj];
     }
-
-    curr_route_cost += vrp.get_distance_on_the_fly(vrp.depot, final_routes[ii][final_routes[ii].size() - 1]);
-
-    total_cost += curr_route_cost;
+    OUTPUT_FILE << '\n';
   }
 
-  return total_cost;
+  OUTPUT_FILE << "Cost " << final_cost << std::endl;
 }
 
-//
-// MAIN POST PROCESS ROUTINE
-//
-std::vector<std::vector<node_t>>
-postProcessIt(const CVRP &vrp, const std::vector<std::vector<node_t>> &final_routes, double& final_cost) {
-  std::vector<std::vector<node_t>> postprocessed_final_routes;
+class Edge
+{
+public:
+    node_t v;
+    weight_t w;
 
-  auto postprocessed_final_routes1 = postprocess_tsp_approx(vrp, final_routes);
-  auto postprocessed_final_routes2 = postprocess_2OPT(vrp, postprocessed_final_routes1);
-  auto postprocessed_final_routes3 = postprocess_2OPT(vrp, final_routes);
+    Edge(node_t _v, weight_t _w): v(_v), w(_w) {}
+};
 
-  //~ weight_t postprocessed_final_routes_cost;
-  double min_cost = 0.0;
-  for (unsigned zzz = 0; zzz < final_routes.size(); ++zzz) {
-    // include the better route between postprocessed_final_routes2[zzz] and postprocessed_final_routes3[zzz] in the final solution.
-    std::vector<node_t> preprocessed_route = final_routes[zzz];
-    std::vector<node_t> postprocessed_route2 = postprocessed_final_routes2[zzz];
-    std::vector<node_t> postprocessed_route3 = postprocessed_final_routes3[zzz];
-
-    unsigned sz = preprocessed_route.size();
-    unsigned sz2 = postprocessed_route2.size();
-    unsigned sz3 = postprocessed_route3.size();
-
-    if(sz != sz2 || sz != sz3)
+void print_graph(const std::vector<std::vector<Edge>>& G)
+{
+    for(node_t u = 0; u < G.size(); u++)
     {
-      HANDLE_ERROR("Sizes are not matching for route in prost processing");
+        OUTPUT_FILE << "Node " << u << ": ";
+        for(const auto& e : G[u])
+        {
+            OUTPUT_FILE << "(" << e.v << ", " << e.w << ") ";
+        }
+        OUTPUT_FILE << "\n";
     }
-    // finding the cost of preprocessed_route
-    weight_t preprocessed_route_cost = 0.0;
-    preprocessed_route_cost += vrp.get_distance_on_the_fly(vrp.depot, preprocessed_route[0]);
-    for(int i = 1; i < sz; i++){
-      preprocessed_route_cost += vrp.get_distance_on_the_fly(preprocessed_route[i-1], preprocessed_route[i]);
-    }
-    preprocessed_route_cost += vrp.get_distance_on_the_fly(preprocessed_route[sz-1], vrp.depot);
-
-    // finding the cost of postprocessed_route2
-    weight_t postprocessed_route2_cost = 0.0;
-    postprocessed_route2_cost += vrp.get_distance_on_the_fly(vrp.depot, postprocessed_route2[0]);  // computing distance of the first point in the route with the depot.
-    for (unsigned jj = 1; jj < sz2; ++jj) {
-      postprocessed_route2_cost += vrp.get_distance_on_the_fly(postprocessed_route2[jj - 1], postprocessed_route2[jj]);
-    }
-    postprocessed_route2_cost += vrp.get_distance_on_the_fly(postprocessed_route2[sz2 - 1], vrp.depot);
-
-    // finding the cost of postprocessed_route3
-    weight_t postprocessed_route3_cost = 0.0;
-    postprocessed_route3_cost += vrp.get_distance_on_the_fly(vrp.depot, postprocessed_route3[0]);
-    for (unsigned jj = 1; jj < sz3; ++jj) {
-      postprocessed_route3_cost += vrp.get_distance_on_the_fly(postprocessed_route3[jj - 1], postprocessed_route3[jj]);
-    }
-    postprocessed_route3_cost += vrp.get_distance_on_the_fly(postprocessed_route3[sz3 - 1], vrp.depot);
-
-    // postprocessed_route2_cost is lower
-    if (std::min(postprocessed_route3_cost, preprocessed_route_cost) >= postprocessed_route2_cost) {
-      postprocessed_final_routes.push_back(postprocessed_route2);
-      min_cost += postprocessed_route2_cost;
-    }
-    // postprocessed_route3_cost is lower
-    else if(std::min(postprocessed_route2_cost, preprocessed_route_cost) >= postprocessed_route3_cost) {
-      postprocessed_final_routes.push_back(postprocessed_route3);
-      min_cost += postprocessed_route3_cost;
-    }else{
-      postprocessed_final_routes.push_back(preprocessed_route);
-      min_cost += preprocessed_route_cost;
-    }
-  }
-  final_cost = min_cost;
-  return postprocessed_final_routes;
 }
 
-
-
-bool verify_sol(const CVRP &vrp, std::vector<std::vector<node_t>> final_routes, unsigned capacity) {
-  /* verifies if the solution is valid or not */
-  /**
-   * 1. All vertices appear in the solution exactly once.
-   * 2. For every route, the capacity constraint is respected.
-   **/
-
-  unsigned *hist = (unsigned *)malloc(sizeof(unsigned) * vrp.size);
-  memset(hist, 0, sizeof(unsigned) * vrp.size);
-
-  for (unsigned i = 0; i < final_routes.size(); ++i) {
-    unsigned route_sum_of_demands = 0;
-    for (unsigned j = 0; j < final_routes[i].size(); ++j) {
-      //~ route_sum_of_demands += points.demands[final_routes[i][j]];
-      route_sum_of_demands += vrp.node[final_routes[i][j]].demand;
-      hist[final_routes[i][j]] += 1;
+class Vector {
+public:
+    cord_t x, y; // x i + y j vector in 2-D plane
+    Vector() : x(0), y(0) {} // Default constructor for a zero vector
+    Vector(cord_t _x, cord_t _y) : x(_x), y(_y) {} // Constructor for a vector with given coordinates
+    // Constructor: Vector pointing towards (x2, y2) from (x1, y1)
+    Vector(cord_t x1, cord_t y1, cord_t x2, cord_t y2) {
+        x = x2 - x1;
+        y = y2 - y1;
     }
-    if (route_sum_of_demands > capacity) {
-      return false;
-    }
-  }
 
-  for (unsigned i = 1; i < vrp.size; ++i) {
-    if (hist[i] > 1) {
-      return false;
+    // Constructor: new vector by rotating vector `v` by `theta` radians
+    Vector(const Vector& v, cord_t theta_rad) {
+        x = v.x * std::cos(theta_rad) - v.y * std::sin(theta_rad);
+        y = v.x * std::sin(theta_rad) + v.y * std::cos(theta_rad);
     }
-    if (hist[i] == 0) {
-      return false;
+
+    // For checking whether this vector is in between two given vectors
+    bool is_in_between(const Vector& vec1, const Vector& vec2) const 
+    {
+        // Compute cross products
+        cord_t cross12 = vec1.x * vec2.y - vec1.y * vec2.x; // vec1 × vec2
+        cord_t cross1p = vec1.x * y - vec1.y * x; // vec1 × vecp
+        cord_t crossp2 = x * vec2.y - y * vec2.x; // vecp × vec2
+        if (cross12 == 0) {
+            // vec1 and vec2 are collinear
+            if (vec1.x * vec2.x + vec1.y * vec2.y >= 0) { // Same direction or one is zero vector
+                // If vecp is also collinear with them, and in the same general direction:
+                return cross1p == 0 && (vec1.x * x + vec1.y * y >= 0); // Check if vecp is also in the same direction as vec1
+            } else { // Opposite directions (180 degrees apart)
+                // ambigious case
+                return cross1p <= 0 && crossp2 <= 0; 
+            }
+        }
+
+        if (cross12 > 0) {
+          return cross1p >= 0 && crossp2 >= 0;  // vecp lies between vec1 and vec2 (angle < 180°)
+        } else {
+          return cross1p <= 0 && crossp2 <= 0;  // reflex angle case   
+        }
     }
-  }
-  return true;
-}
+};
